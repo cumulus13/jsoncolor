d1ddee8544bb2d38fe72d8e54fae9b6e8059b748
cumulus13 <cumulus13@gmail.com>
2022-12-06 12:51:46 +0700

    version: 0.14 ~ 2022-12-06 12:51:44:278538

--- jsoncolor/jsoncolor.py
+++ jsoncolor/jsoncolor.py
@@ -4,8 +4,13 @@ import sys
 import json
 import argparse
 import clipboard
+import ast
 
 def jprint(data, sort=True, indent = 4):
+	try:
+		if isinstance(data, str): data = json.loads(data)
+	except:
+		if isinstance(data, str): data = ast.literal_eval(data)
 	if sort == None: sort = True
 	data = json.dumps(data, sort_keys=sort, indent = indent)
 	if sys.version_info.major == 3:
@@ -37,4 +42,5 @@ def usage(text = None):
 
 if __name__ == '__main__':
 	data_test = [{'Status:': {'value': [' Ongoing'], 'links': [], 'time': []}}, {'Network:': {'value': [' Bilibili'], 'links': [{'name': 'Bilibili', 'link': 'https://anichin.vip/network/bilibili/'}], 'time': []}}, {'Studio:': {'value': [' Pb Animation Co. Ltd.'], 'links': [{'name': 'Pb Animation Co. Ltd.', 'link': 'https://anichin.vip/studio/pb-animation-co-ltd/'}], 'time': []}}, {'Tanggal rilis:': {'value': [' Oct 02, 2022'], 'links': [], 'time': []}}, {'Durasi:': {'value': [' 20 min per ep'], 'links': [], 'time': []}}, {'Season:': {'value': [' Fall 2022'], 'links': [{'name': 'Fall 2022', 'link': 'https://anichin.vip/season/fall-2022/'}], 'time': []}}, {'Negara:': {'value': [' China'], 'links': [{'name': 'China', 'link': 'https://anichin.vip/country/china/'}], 'time': []}}, {'Tipe:': {'value': [' Donghua'], 'links': [], 'time': []}}, {'Episode:': {'value': [' 12'], 'links': [], 'time': []}}, {'Diterjemahkan oleh:': {'value': [' Dongdong'], 'links': [], 'time': []}}, {'Diposting oleh:': {'value': [' Dongdong'], 'links': [], 'time': []}}, {'Ditambahkan:': {'value': [' September 30, 2022'], 'links': [], 'time': [{'name': 'September 30, 2022', 'time': '2022-09-30T21:52:13+07:00'}]}}, {'Terakhir diedit:': {'value': [' December 4, 2022'], 'links': [], 'time': [{'name': 'December 4, 2022', 'time': '2022-12-04T14:28:09+07:00'}]}}]
-	usage(data_test)
+	#usage(data_test)
+	usage()
